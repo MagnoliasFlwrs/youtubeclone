b408aa633768f97bb4a078e84090867ba6da3bd0
Marta Savianok <magnolia.sav@gmail.com>
2022-12-15 13:29:57 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -5,36 +5,26 @@ import MainLayout from "./components/layout/MainLayout";
 import HomeVideoScreen from "./components/HomeVideoScreen";
 import { useSelector } from "react-redux";
 import ViewVideoLayout from "./components/layout/ViewVideoLayout";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import SearchLayout from './components/layout/SearchLayout'
-
-
-
-
+import SearchLayout from "./components/layout/SearchLayout";
 
 function App() {
-  const navigate = useNavigate()
-  const token = useSelector(state => state.auth.token)
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
   useEffect(() => {
-    if(!token) {
-      navigate('auth')
+    if (!token) {
+      navigate("auth");
     }
-  },[token])
+  }, [token]);
   return (
-    <GoogleOAuthProvider>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="" element={<HomeVideoScreen />} />
-          <Route path="search/:query" element={<SearchLayout />} />
-        </Route>
-        <Route path="watch/:id" element={<ViewVideoLayout/>}/>
-        <Route path="auth" element={<LoginLayout />} />
-         
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<HomeVideoScreen />} />
+        <Route path="search/:query" element={<SearchLayout />} />
+      </Route>
+      <Route path="watch/:id" element={<ViewVideoLayout />} />
+      <Route path="auth" element={<LoginLayout />} />
     </Routes>
-    </GoogleOAuthProvider>
-
   );
 }
-
 
 export default App;
